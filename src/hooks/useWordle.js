2661d28d5aa97f01fbf8d0d33e260edbffff84a2
moad dabit <moad.dabit@ajicreative.club>
2022-06-03 10:18:54 +0100

    Submitting Guesses

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -4,7 +4,7 @@ const useWordle = (solution) => {
   const [turn, setTurn] = useState(0);
   const [currentGuess, setCurrentGuess] = useState("");
   const [guesses, setGuesses] = useState([]); // each guess is an array
-  const [history, setHistory] = useState([]); // each guess is a string
+  const [history, setHistory] = useState(["hello", "ninja"]); // each guess is a string
   const [isCorrect, setIsCorrect] = useState(false);
 
   /*
@@ -12,7 +12,9 @@ const useWordle = (solution) => {
    * e.g [{key: 'a. color: 'yellow}]
    */
 
-  const formatGuess = () => {};
+  const formatGuess = () => {
+    console.log("formatting the guess - ", currentGuess);
+  };
 
   /*
    * add a new guess to the guesses state
@@ -28,12 +30,36 @@ const useWordle = (solution) => {
    */
 
   const handleKeyUp = ({ key }) => {
+    if (key === "Enter") {
+      //* only add guess if turn is less than 5
+      if (turn > 5) {
+        console.log("you used all your guesses");
+        return;
+      }
+      //* di not allow duplicate words
+      if (history.includes(currentGuess)) {
+        console.log("you already tried taht word");
+        return;
+      }
+      //* check word is 5 char long
+      if (currentGuess.length !== 5) {
+        console.log("word must be 5 chars long");
+        return;
+      }
+
+      if (key === "Enter") {
+      }
+      formatGuess();
+    }
+
     if (key === "Backspace") {
       setCurrentGuess((prev) => {
+        // slice methode slected element in array by start given.
         return prev.slice(0, -1);
       });
       return;
     }
+
     if (/^[a-zA-Z]$/.test(key)) {
       if (currentGuess.length < 5) {
         setCurrentGuess((prev) => {
