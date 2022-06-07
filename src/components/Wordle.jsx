1a34964572bfe7e388d35919500a41b641a7d00e
moad dabit <moad.dabit@ajicreative.club>
2022-06-07 11:07:06 +0100

    making modal

--- src/components/Wordle.jsx
+++ src/components/Wordle.jsx
@@ -1,22 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useWordle from "../hooks/useWordle";
 import Grid from "./Grid";
 import Keypad from "./Keypad";
+import Modal from "./Modal";
 
 const Wordle = ({ solution }) => {
   const { currentGuess, handleKeyUp, isCorrect, turn, guesses, usedKeys } =
     useWordle(solution);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     window.addEventListener("keyup", handleKeyUp);
 
+    if (isCorrect) {
+      setTimeout(() => setShowModal(true), 2000);
+      window.addEventListener("keyup", handleKeyUp);
+    }
+
+    if (turn > 5) {
+      setTimeout(() => setShowModal(true), 2000);
+      window.addEventListener("keyup", handleKeyUp);
+    }
+
     // return cleaup function and run it as a first
     return () => window.removeEventListener("keyup", handleKeyUp);
-  }, [handleKeyUp]);
+  }, [handleKeyUp, turn, isCorrect]);
 
-  useEffect(() => {
-    console.log(guesses, turn, isCorrect);
-  }, [guesses, turn, isCorrect]);
+  // useEffect(() => {
+  //   console.log(guesses, turn, isCorrect);
+  // }, [guesses, turn, isCorrect]);
 
   return (
     <>
@@ -24,6 +36,9 @@ const Wordle = ({ solution }) => {
       <div>current Guess - {currentGuess}</div>
       <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
       <Keypad usedKeys={usedKeys} />
+      {showModal && (
+        <Modal isCorrect={isCorrect} turn={turn} solution={solution} />
+      )}
     </>
   );
 };
